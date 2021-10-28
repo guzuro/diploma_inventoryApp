e4298a23751bfd0e02f418686f14f9b1498c77f7
guzuro <guzurogmn@gmail.com>
2021-10-28 23:18:38 +0300

    added category

---
+++ src/main/frontend/src/store/modules/categories/state.ts
@@ -0,0 +1,34 @@
+import { Category } from '@/types/Category';
+
+export interface State {
+  categories: Category[];
+}
+
+const state: State = {
+  categories: [{
+    id: 1,
+    name: 'Кисломолочка',
+  }, {
+    id: 2,
+    name: 'Выпечка',
+  }, {
+    id: 3,
+    name: 'Полуфабрикаты',
+  }, {
+    id: 4,
+    name: 'Полуфабрикаты',
+  }, {
+    id: 5,
+    name: 'Макароны и крупы',
+  }],
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations: {
+  },
+  actions: {
+  },
+  getters: {}
+};
