56168903433c1e4fd7d247cf42cd89d74b77aa2f
guzuro <guzurogmn@gmail.com>
2022-03-02 23:55:18 +0300

    added roles models

---
+++ src/main/java/com/guzuro/Models/Roles/Administrator.java
@@ -0,0 +1,7 @@
+package com.guzuro.Models.Roles;
+
+import com.guzuro.Daos.UserDao.User;
+
+public class Administrator extends User {
+    Employee[] employes;
+}
---
+++ src/main/java/com/guzuro/Models/Roles/Employee.java
@@ -0,0 +1,7 @@
+package com.guzuro.Models.Roles;
+
+import com.guzuro.Daos.UserDao.User;
+
+public class Employee extends User {
+
+}
