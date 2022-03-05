f9fb974d76b07d117def86be1755d0cdac7a7225
guzuro <guzurogmn@gmail.com>
2022-03-05 23:58:13 +0300

    element instead of buefy

--- src/main/frontend/src/main.ts
+++ src/main/frontend/src/main.ts
@@ -1,17 +1,20 @@
-import Buefy from 'buefy';
 import VueApexCharts from 'vue-apexcharts';
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { extend } from 'vee-validate';
 import { email, required, min } from 'vee-validate/dist/rules';
+import Antd from 'ant-design-vue';
 import App from './App.vue';
 import router from './router/router';
 import store from './store';
-import 'buefy/dist/buefy.css';
 import './assets/styles.scss';
 
-Vue.use(Buefy);
+import 'ant-design-vue/dist/antd.css';
+import '@/assets/tailwind.css';
+
+Vue.use(Antd);
+
 Vue.use(VueApexCharts);
 Vue.use(VueAxios, axios);
 
