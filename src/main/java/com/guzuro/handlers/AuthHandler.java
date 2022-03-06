31f2c054d236f120c4320559d8faa38f5919ffde
guzuro <guzurogmn@gmail.com>
2022-03-06 23:31:35 +0300

    / back / register handler update

--- src/main/java/com/guzuro/handlers/AuthHandler.java
+++ src/main/java/com/guzuro/handlers/AuthHandler.java
@@ -3,6 +3,9 @@ package com.guzuro.handlers;
 import com.guzuro.Daos.AuthDao.AuthDao;
 import com.guzuro.Daos.AuthDao.LoginData;
 import com.guzuro.Daos.AuthDao.PostgresAuthDaoImpl;
+import com.guzuro.Daos.CompanyDao.Company;
+import com.guzuro.Daos.CompanyDao.CompanyDao;
+import com.guzuro.Daos.CompanyDao.PostgresCompanyDaoImpl;
 import com.guzuro.Daos.UserDao.User;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
@@ -12,30 +15,42 @@ import io.vertx.ext.web.Session;
 
 public class AuthHandler {
 
-    AuthDao dao;
+    AuthDao authDao;
+    CompanyDao companyDao;
 
     public AuthHandler(Vertx vertx) {
-        this.dao = new PostgresAuthDaoImpl(vertx);
+        this.authDao = new PostgresAuthDaoImpl(vertx);
+        this.companyDao = new PostgresCompanyDaoImpl(vertx);
     }
 
     public void register(RoutingContext context) {
         HttpServerResponse response = context.response();
         User reqUser = context.getBodyAsJson().mapTo(User.class);
         reqUser.setRole("Administrator");
-        this.dao.register(reqUser).thenAccept(resUser -> {
-            Session session = context.session();
-            session.put("id", resUser.getId());
+        this.authDao.register(reqUser)
+                .thenAccept(user -> {
+                    Company company = new Company(user.getCompany());
 
-            response.putHeader("content-type", "application/json; charset=UTF-8")
-                    .setStatusCode(201)
-                    .end(JsonObject.mapFrom(resUser).encodePrettily());
-        }).exceptionally(throwable -> {
-            JsonObject error = new JsonObject();
-            error.put("error", throwable.getCause().getMessage());
+                    this.companyDao.createCompany(company)
+                            .thenAccept(resCompany -> {
+                                Session session = context.session();
+                                session.put("id", user.getId());
 
+                                response.putHeader("content-type", "application/json; charset=UTF-8")
+                                        .setStatusCode(201)
+                                        .end(JsonObject.mapFrom(user).encodePrettily());
+
+                            })
+                            .exceptionally(throwable -> {
+                                response.setStatusCode(500)
+                                        .putHeader("content-type", "application/json; charset=UTF-8")
+                                        .end(throwable.getCause().getMessage());
+                                return null;
+                            });
+                }).exceptionally(throwable -> {
             response.setStatusCode(500)
                     .putHeader("content-type", "application/json; charset=UTF-8")
-                    .end(error.encodePrettily());
+                    .end(throwable.getCause().getMessage());
             return null;
         });
     }
@@ -45,7 +60,7 @@ public class AuthHandler {
         HttpServerResponse response = context.response();
         LoginData loginData = context.getBodyAsJson().mapTo(LoginData.class);
 
-        this.dao.login(loginData).thenAccept(resUser -> {
+        this.authDao.login(loginData).thenAccept(resUser -> {
 
             Session session = context.session();
             if (session.get("id") == null) {
@@ -72,19 +87,19 @@ public class AuthHandler {
         if (session.get("id") == null) {
             response.setStatusCode(401).end();
         } else {
-           this.dao.getUserById(session.get("id")).thenAccept(resUser -> {
-               response.putHeader("content-type", "application/json; charset=UTF-8")
-                       .setStatusCode(200)
-                       .end(JsonObject.mapFrom(resUser).encodePrettily());
+            this.authDao.getUserById(session.get("id")).thenAccept(resUser -> {
+                response.putHeader("content-type", "application/json; charset=UTF-8")
+                        .setStatusCode(200)
+                        .end(JsonObject.mapFrom(resUser).encodePrettily());
 
-           }).exceptionally(throwable -> {
-               if (throwable.getMessage().contains("NOT FOUND")) {
-                   response.setStatusCode(404).end();
-               } else {
-                   response.setStatusCode(500).end(throwable.getMessage());
-               }
-               return null;
-           });
+            }).exceptionally(throwable -> {
+                if (throwable.getMessage().contains("NOT FOUND")) {
+                    response.setStatusCode(404).end();
+                } else {
+                    response.setStatusCode(500).end(throwable.getMessage());
+                }
+                return null;
+            });
         }
     }
 
