0ac4250418573abd46fe5b8a4e8a81e1cb2d000d
guzuro <guzurogmn@gmail.com>
2022-03-09 23:41:56 +0300

    / fr / updated main user info form

--- src/main/frontend/src/components/Auth/RegistrationForm.vue
+++ src/main/frontend/src/components/Auth/RegistrationForm.vue
@@ -3,100 +3,13 @@
     <p class="is-size-3 has-text-centered mt-5">Регистрация</p>
     <ValidationObserver v-slot="{ invalid }">
       <div class="flex flex-col">
-        <ValidationProvider
-          name="E-mail"
-          immediate
-          rules="required|email"
-          v-slot="{ errors }"
-        >
-          <a-input
-            class="mt-5"
-            placeholder="Email"
-            maxlength="30"
-            v-model="user.email"
-          >
-          </a-input>
-          <span class="text-red-900"> {{ errors[0] }} </span>
-        </ValidationProvider>
-        <ValidationProvider
-          name="Фамилия"
-          immediate
-          rules="required"
-          v-slot="{ errors }"
-        >
-          <a-input
-            class="mt-5"
-            placeholder="Фамилия"
-            type="text"
-            v-model="user.last_name"
-          >
-          </a-input>
-          <span class="text-red-900"> {{ errors[0] }} </span>
-        </ValidationProvider>
-        <ValidationProvider
-          name="Номер телефона"
-          immediate
-          rules="required"
-          v-slot="{ errors }"
-        >
-          <a-input
-            placeholder="Номер телефона"
-            class="mt-5"
-            type="text"
-            v-model="user.phone"
-          >
-          </a-input>
-          <span class="text-red-900"> {{ errors[0] }} </span>
-        </ValidationProvider>
-        <ValidationProvider
-          name="Пароль"
-          immediate
-          rules="required|min:7"
-          v-slot="{ errors }"
-        >
-          <a-input
-            class="mt-5"
-            placeholder="Пароль"
-            type="password"
-            password-reveal
-            v-model="user.password"
-          >
-          </a-input>
-          <span class="text-red-900"> {{ errors[0] }} </span>
-        </ValidationProvider>
-        <ValidationProvider
-          name="Имя"
-          immediate
-          rules="required"
-          v-slot="{ errors }"
-        >
-          <a-input
-            class="mt-5"
-            placeholder="Имя"
-            type="text"
-            v-model="user.first_name"
-          >
-          </a-input>
-          <span class="text-red-900"> {{ errors[0] }} </span>
-        </ValidationProvider>
+        <main-user-info-form v-model="user" />
 
-        <ValidationProvider
-          name="Название компании / ИП"
-          immediate
-          rules="required"
-          v-slot="{ errors }"
-        >
-          <a-input
-            placeholder="Название компании / ИП"
-            type="text"
-            class="mt-5"
-            v-model="user.company"
-          />
+        <ValidationProvider name="Название компании / ИП" immediate rules="required" v-slot="{ errors }">
+          <a-input placeholder="Название компании / ИП" type="text" class="mt-5" v-model="company" />
           <span class="text-red-900"> {{ errors[0] }} </span>
         </ValidationProvider>
-        <a-button :disabled="invalid" class="ml-auto mt-5" @click="register">
-          Регистрация
-        </a-button>
+        <a-button :disabled="invalid" class="ml-auto mt-5" @click="register"> Регистрация </a-button>
       </div>
     </ValidationObserver>
   </div>
@@ -108,11 +21,13 @@ import { Component } from 'vue-property-decorator';
 import { ValidationObserver, ValidationProvider } from 'vee-validate';
 import { User } from '@/types/User';
 import AuthService from '@/services/AuthService';
+import MainUserInfoForm from '../MainUserInfoForm.vue';
 
 @Component({
   components: {
     ValidationObserver,
     ValidationProvider,
+    MainUserInfoForm,
   },
 })
 export default class RegistrationForm extends Vue {
@@ -124,7 +39,7 @@ export default class RegistrationForm extends Vue {
     phone: '',
   };
 
-  company = ''
+  company = '';
 
   async register(): Promise<void> {
     const res = await AuthService.register({ user: this.user, company: this.company });
---
+++ src/main/frontend/src/components/MainUserInfoForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main-user-info-form">
+    <ValidationProvider name="E-mail" immediate rules="required|email" v-slot="{ errors }">
+      <field-wrapper class="mt-5" :fieldTitle="'E-mail'">
+        <a-input placeholder="Email" maxlength="30" v-model="user.email"> </a-input>
+        <span class="text-red-900"> {{ errors[0] }} </span>
+      </field-wrapper>
+    </ValidationProvider>
+    <ValidationProvider name="Фамилия" immediate rules="required" v-slot="{ errors }">
+      <field-wrapper class="mt-5" :fieldTitle="'Фамилия'">
+        <a-input placeholder="Фамилия" type="text" v-model="user.last_name"> </a-input>
+        <span class="text-red-900"> {{ errors[0] }} </span>
+      </field-wrapper>
+    </ValidationProvider>
+    <ValidationProvider name="Номер телефона" immediate rules="required" v-slot="{ errors }">
+      <field-wrapper :fieldTitle="'Номер телефона'" class="mt-5">
+        <a-input placeholder="Номер телефона" type="text" v-model="user.phone"> </a-input>
+        <span class="text-red-900"> {{ errors[0] }} </span>
+      </field-wrapper>
+    </ValidationProvider>
+    <ValidationProvider name="Пароль" immediate rules="required|min:7" v-slot="{ errors }">
+      <field-wrapper class="mt-5" :fieldTitle="'Пароль'">
+        <a-input placeholder="Пароль" type="password" password-reveal v-model="user.password"> </a-input>
+        <span class="text-red-900"> {{ errors[0] }} </span>
+      </field-wrapper>
+    </ValidationProvider>
+    <ValidationProvider name="Имя" immediate rules="required" v-slot="{ errors }">
+      <field-wrapper class="mt-5" :fieldTitle="'Имя'">
+        <a-input placeholder="Имя" type="text" v-model="user.first_name"> </a-input>
+        <span class="text-red-900"> {{ errors[0] }} </span>
+      </field-wrapper>
+    </ValidationProvider>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, VModel } from 'vue-property-decorator';
+import { ValidationProvider } from 'vee-validate';
+import { User } from '@/types/User';
+import FieldWrapper from './FieldWrapper.vue';
+
+@Component({
+  components: {
+    FieldWrapper,
+    ValidationProvider,
+  },
+})
+export default class MainUserInfoForm extends Vue {
+  @VModel() user!: User;
+}
+</script>
