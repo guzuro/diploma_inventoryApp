7532dfe02d0d5885dd52d075772c42c5688799c9
guzuro <guzurogmn@gmail.com>
2022-03-21 23:41:42 +0300

    /fr/ updated routes

--- src/main/frontend/src/router/routes.ts
+++ src/main/frontend/src/router/routes.ts
@@ -7,58 +7,64 @@ const routes: Array<RouteConfig> = [
     name: 'Login',
   },
   {
+    component: () => import('../layouts/OfficeLayout.vue'),
+    path: '/office/',
+    children: [
+      {
+        component: () => import('../views/general/profile.vue'),
+        path: ':userId/profile',
+        name: 'Profile',
+      },
+      {
+        component: () => import('../views/general/company-profile.vue'),
+        path: ':userId/company-profile',
+        name: 'CompanyProfile',
+      },
+      {
+        component: () => import('../views/employes/employes.vue'),
+        path: ':userId/employes',
+        name: 'Employes',
+      },
+      {
+        component: () => import('../views/config/warehouses.vue'),
+        path: ':userId/warehouses',
+        name: 'Warehouses',
+      },
+      {
+        component: () => import('../views/config/categories.vue'),
+        path: ':userId/categories',
+        name: 'Categories',
+      },
+      {
+        component: () => import('../views/config/sales.vue'),
+        path: ':userId/sales',
+        name: 'Sales',
+      },
+      {
+        component: () => import('../views/config/employee-roles.vue'),
+        path: ':userId/employee-roles',
+        name: 'EmployeeRoles',
+      },
+      {
+        component: () => import('../views/product-list.vue'),
+        path: ':userId/products',
+        name: 'Products',
+      },
+      {
+        component: () => import('../views/product.vue'),
+        path: ':userId/products/:actionType',
+        name: 'Product',
+      },
+    ],
+  },
+  {
     component: () => import('../layouts/DashboardLayout.vue'),
     path: '',
     children: [
       {
         component: () => import('../views/dashboard.vue'),
-        path: '/office/:userId/dashboard/',
+        path: '',
         name: 'Dashboard',
-      },
-      {
-        component: () => import('../views/general/profile.vue'),
-        path: '/office/:userId/profile',
-        name: 'Profile',
-      },
-      {
-        component: () => import('../views/general/company-profile.vue'),
-        path: '/office/:userId/company-profile',
-        name: 'CompanyProfile',
-      },
-      {
-        component: () => import('../views/employes/employes.vue'),
-        path: '/office/:userId/employes',
-        name: 'Employes',
-      },
-      {
-        component: () => import('../views/config/warehouses.vue'),
-        path: '/office/:userId/warehouses',
-        name: 'Warehouses',
-      },
-      {
-        component: () => import('../views/config/categories.vue'),
-        path: '/office/:userId/categories',
-        name: 'Categories',
-      },
-      {
-        component: () => import('../views/config/sales.vue'),
-        path: '/office/:userId/sales',
-        name: 'Sales',
-      },
-      {
-        component: () => import('../views/config/employee-roles.vue'),
-        path: '/office/:userId/employee-roles',
-        name: 'EmployeeRoles',
-      },
-      {
-        component: () => import('../views/product-list.vue'),
-        path: '/office/:userId/products',
-        name: 'Products',
-      },
-      {
-        component: () => import('../views/product.vue'),
-        path: '/office/:userId/products/:actionType',
-        name: 'Product',
       },
     ],
   },
