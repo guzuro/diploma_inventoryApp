6759b4642017dbf81d2da78ca1f04fc1156bcfa6
guzuro <guzurogmn@gmail.com>
2022-04-15 01:04:36 +0300

    /fr/ api fix

--- src/main/frontend/src/services/UploadFileService.ts
+++ src/main/frontend/src/services/UploadFileService.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { errorNotification } from './NotificationService';
+import BaseApi from '@/services/BaseApi';
 
 export default class UploadFileService {
   static async uploadFile(file: any): Promise<Array<string>> {
     try {
-      const { data } = await axios.post('http://localhost:8888/uploadfile', file, {
+      const { data } = await axios.post(`${BaseApi.BASE_API}/uploadfile`, file, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
