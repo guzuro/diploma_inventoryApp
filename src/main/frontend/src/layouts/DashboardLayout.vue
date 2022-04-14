226240e19c07d0f39796d714406f7673f156a56d
guzuro <guzurogmn@gmail.com>
2022-04-14 23:56:34 +0300

    /fr/ layout update

--- src/main/frontend/src/layouts/DashboardLayout.vue
+++ src/main/frontend/src/layouts/DashboardLayout.vue
@@ -2,22 +2,21 @@
   <a-layout id="components-layout-demo-top" class="layout">
     <a-layout-header>
       <div class="logo" />
-      <a-menu theme="dark" mode="horizontal" :default-selected-keys="activeKey" :style="{ lineHeight: '42px' }">
-        <a-menu-item key="Dashboard" @click="goto('Dashboard')"> <a-icon type="appstore" />Dashboard </a-menu-item>
-        <a-menu-item key="Products" @click="goto('Products')"> <a-icon type="shop" />Продукты </a-menu-item>
-        <!-- <a-menu-item key="Products" @click="goto('Suppliers')"> <a-icon type="shop" />Поставщики </a-menu-item> -->
-        <a-sub-menu>
-          <span slot="title" class="submenu-title-wrapper"><a-icon type="setting" />Закупки</span>
-          <a-menu-item-group>
-            <a-menu-item key="setting:1" @click="goto('Suppliers')"> Поставщики </a-menu-item>
-            <a-menu-item key="setting:2" @click="goto('IncomeDocs')"> Приходные накладные </a-menu-item>
-          </a-menu-item-group>
-          <!-- <a-menu-item-group title="Item 2">
-            <a-menu-item key="setting:3"> Option 3 </a-menu-item>
-            <a-menu-item key="setting:4"> Option 4 </a-menu-item>
-          </a-menu-item-group> -->
-        </a-sub-menu>
-      </a-menu>
+      <div class="flex justify-between items-center">
+        <a-menu theme="dark" mode="horizontal" :default-selected-keys="activeKey" :style="{ lineHeight: '64px' }">
+          <a-menu-item key="Dashboard" @click="goto('Dashboard')"> <a-icon type="appstore" />Dashboard </a-menu-item>
+          <a-menu-item key="Products" @click="goto('Products')"> <a-icon type="shop" />Продукты </a-menu-item>
+          <a-sub-menu>
+            <span slot="title" class="submenu-title-wrapper"><a-icon type="setting" />Закупки</span>
+            <a-menu-item-group>
+              <a-menu-item key="setting:1" @click="goto('Suppliers')"> Поставщики </a-menu-item>
+              <a-menu-item key="setting:2" @click="goto('IncomeDocs')"> Приходные накладные </a-menu-item>
+            </a-menu-item-group>
+          </a-sub-menu>
+        </a-menu>
+
+        <a-button type="primary" icon="user" @click="handleUserIconClick" />
+      </div>
     </a-layout-header>
     <a-layout-content class="mt-5" style="padding: 0 50px">
       <a-breadcrumb class="mb-5">
@@ -56,6 +55,15 @@ export default class DashboardLayout extends Vue {
     });
   }
 
+  handleUserIconClick(): void {
+    this.$router.push({
+      name: 'Profile',
+      params: {
+        userId: this.$store.state.userModule.userData.id,
+      },
+    });
+  }
+
   created(): void {
     setInterval(() => {
       this.currentTime = moment().format('YYYY-MM-DD, HH:mm');
@@ -68,7 +76,7 @@ export default class DashboardLayout extends Vue {
 // .ant-layout {
 //   height: 100vh;
 // }
-.ant-layout-header {
-  height: 45px;
-}
+// .ant-layout-header {
+//   height: px;
+// }
 </style>
